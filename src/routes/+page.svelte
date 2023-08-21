7968bb3cb01bf4d8cbbc637cd01e38181fcf153b
Kargisa <luca.wolf@outlook.com>
2023-08-21 15:25:27 +0200

    basic implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
 <script lang="ts">
 	import BingBong from '../components/BingBong.svelte';
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn btn-primary">bingbong</button>
-
-<BingBong />
---
+++ src/routes/Bookings/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { bookings } from '$lib/bookings';
+
+	const deleteBooking = (id: number) => {
+		bookings.update((b) => b.filter((b) => b.id !== id));
+	};
+</script>
+
+<div class="overflow-x-auto">
+	<table class="table">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Date</th>
+				<th>Time</th>
+				<th />
+			</tr>
+		</thead>
+		<tbody>
+			{#each $bookings as booking (booking.id)}
+				<tr>
+					<th>{booking.id}</th>
+					<td>{booking.date}</td>
+					<td>{booking.time}</td>
+					<td on:click={() => deleteBooking(booking.id)}> Delete </td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
