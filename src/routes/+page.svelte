82e9f9ed001a7ccd33e5b277dc8ee95f749d650c
Kargisa <luca.wolf@outlook.com>
2023-08-24 14:57:36 +0200

    Booking deletion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,19 @@
 	import Check from '$components/Check.svelte';
 	import CrazyAnimation from '$components/CrazyAnimation.svelte';
 	import DateSelection from '$components/DateSelection.svelte';
-	import { bookings } from '$lib/bookings';
 	import type { PageData } from './$types';
 
 	let visible = false;
 	let triggerEl: any;
-    let panelVisible = false;
- 
-    function togglePanel() {
-      panelVisible = !panelVisible;
-    }
- 
-    function hidePanel() {
-      panelVisible = false;
-    }
+	let panelVisible = false;
+
+	function togglePanel() {
+		panelVisible = !panelVisible;
+	}
+
+	function hidePanel() {
+		panelVisible = false;
+	}
 
 	function spinnnnn() {
 		visible = true;
@@ -23,9 +22,7 @@
 			visible = false;
 		}, 5000);
 	}
-
 </script>
-
 
 <button class="btn btn-primary" on:click={spinnnnn}>bingbong</button>
 
@@ -35,4 +32,4 @@
 	</CrazyAnimation>
 {/if}
 
-<DateSelection></DateSelection>
+<DateSelection />
