450be888fc858a3717c80031221a9c25a11f7d04
LeopoldKainz <kainz.l19@htlwienwest.at>
2023-08-24 11:14:36 +0200

    Call to backend to get all Locations

--- src/routes/location/+page.svelte
+++ src/routes/location/+page.svelte
@@ -1,31 +1,34 @@
 <script lang="ts">
 	import DefaultLocation from '$components/DefaultLocation.svelte';
-	let locations: string[] = ['Wien', 'Salzburg', 'Hagenberg'];
-	//api call to server
-	let chosenLocation: string;
+	import type { PageData } from './$houdini';
+	let chosenLocation: {pk_locationid: string | null, locationname: string | null};
 	let showModal: boolean = false;
 
-	function toggleModal(){
+	function toggleModal() {
 		showModal = !showModal;
 	}
+
+	export let data: PageData;
+	$: ({ getAllLocations } = data);
+	$: locations = $getAllLocations.data?.getAllLocations;
 </script>
 
-<div class="grid pla ce-items-center place-content-center space-y-6 h-screen">
+<div class="grid place-items-center place-content-center space-y-6 h-screen">
 	<h1 class="text-2xl">Choose Your Location:</h1>
-	{#each locations as location}
-		<button
-			class="btn btn-block btn-primary px-14"
-			on:click={() => {
-				chosenLocation = location;
-				toggleModal();
-			}}
-		>
-			<a href="/location/defaultLocation">
-				{location}
-			</a>
-		</button>
-	{/each}
+	{#if locations}
+		{#each locations as location}
+			<button
+				class="btn btn-block btn-primary px-14"
+				on:click={() => {
+					chosenLocation = location ?? {pk_locationid: "", locationname: ""};
+					toggleModal();
+				}}
+			>
+					{location?.locationname}
+			</button>
+		{/each}
+	{/if}
 	{#if showModal}
-		<DefaultLocation {chosenLocation} on:close={toggleModal}  />
+		<DefaultLocation {chosenLocation} on:close={toggleModal} />
 	{/if}
 </div>
