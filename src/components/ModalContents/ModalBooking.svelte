780f17ba2aa19ad5b78bb8a4222d8724937daab4
tobi <tischler.dev@gmail.com>
2023-10-09 16:25:55 +0000

    finished

--- src/components/ModalContents/ModalBooking.svelte
+++ src/components/ModalContents/ModalBooking.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	//amongus
 	import { Stepper, Step } from '@skeletonlabs/skeleton';
 	import { getModalStore } from '@skeletonlabs/skeleton';
 	import { interval, selectedDesk } from '$lib/bookingStore';
