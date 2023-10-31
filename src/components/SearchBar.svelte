7f0a675134b3ed75a5b4a8d0cf5f2c8fd46b3b22
Sofia <0255@htl.rennweg.at>
2023-10-31 14:26:39 +0100

    updated bookingStore, added userInfo to userTypes

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -19,10 +19,10 @@
 		}
 	`);
 
-    //$: users = getUsers.fetch({ variables: { pageNumber: 1, pageSize: 2}});
 
     let pageNumber = 0;
     let searchUsers: User[] = [];
+    let dropdownIsOpen: boolean = false;
     const pageSizeConst = 2;
 
     onMount(() => {
@@ -30,7 +30,7 @@
     })
 
     async function getSearchUsers(pageNumber_param: number) {
-        await getUsers.fetch({ variables: { input: selectedUsername, pageNumber: pageNumber_param, pageSize: pageSizeConst}}).then(() => {
+        await getUsers.fetch({ variables: { input: typedUsername, pageNumber: pageNumber_param, pageSize: pageSizeConst}}).then(() => {
             let users = $getUsers.data?.getAllUsers;
             searchUsers = users.map((user) => ({
                 pk_userid: user?.pk_userid,
@@ -38,13 +38,48 @@
                 location: null
             }));
         })
+
+        for (const user of searchUsers) {
+            onUserClicked(user);
+        }
+
+    }
+
+    // let userInfo: string = "";
+    let userLocation: string = "";
+
+    async function onUserClicked(user: User) {
+        //typedUsername = user?.username ?? "";
+        selectedUser = user;
+        getBookings.fetch({variables: {userid: user.pk_userid ?? ""}});
+        for (const booking of bookingsOfSelectedUser?? []) {
+            //console.log(booking?.date + "== " + $dateValue);
+            if (booking?.date == $dateValue) {
+                //bookingsOnDate.push(booking);       
+                await getDesk.fetch({variables: {bookingid: booking.pk_bookingid ?? ""}}).then(() => {
+                    let desk = $getDesk.data?.getBookingById?.desk;
+                    // console.log(user.username + " sitzt am nachmittag hier: " + booking.isafternoon)
+                    // console.log(user.username + " sitzt heute auf " + desk?.floor?.building?.location?.locationname);  
+                    userLocation = desk?.floor?.building.location?.locationname ?? "";
+                    if (booking.ismorning || booking.isafternoon) user.userInfo = "today in " + userLocation;
+                    else {
+                        console.log(user.username + "not in office today");
+                        user.userInfo = "not in office today"
+                    }
+                    console.log(user.username + " -> userLocation: " + userLocation + "; userInfo: " + user.userInfo);
+                })
+            }
+
+            
+
+        }
     }
 
    
     $: bookingsOfSelectedUser = $getBookings.data?.getBookingsByUserid;
     let bookingsOnDate = [];
 
-    let selectedUsername: string;
+    let typedUsername: string;
     let selectedUser: User;
 
     export const _getDeskOfBookingVariables = () => {
@@ -54,37 +89,40 @@
     const getDesk = graphql(`
         query getDeskOfBooking($bookingid: ID!) @load {
             getBookingById(id: $bookingid) {
+                ismorning
+                isafternoon
                 desk{
                     desknum
                     y
                     x
+                    floor {
+                        pk_floorid
+                        floorname
+                        building {
+                            buildingname
+                            location {
+                                locationname
+                            }
+                        }
+                    }
                 }
             }
         }
     `)
 
-
-    async function onUserClicked(user: User) {
-        selectedUsername = user?.username ?? "";
-        selectedUser = user;        
-        getBookings.fetch({variables: {userid: user.pk_userid ?? ""}});
-        for (const booking of bookingsOfSelectedUser?? []) {
-            console.log(booking?.date + "== " + $dateValue);
-            
-                      
-            if (booking?.date == $dateValue) {
-                bookingsOnDate.push(booking);       
-                await getDesk.fetch({variables: {bookingid: booking.pk_bookingid ?? ""}}).then(() => {
-                    let desk = $getDesk.data?.getBookingById?.desk;
-                    console.log(user.username + " sitzt heute auf " + desk?.desknum);  
-                })
-            }
-        }
+    function handleDropDownClick() {
+        dropdownIsOpen = true;
     }
+
+    function handleDropdownFocusLoss() {
+        dropdownIsOpen = false;
+    }
+
+  
 
     
     $: {
-        if (selectedUsername) {
+        if (typedUsername) {
             getSearchUsers(pageNumber);
         }
     }
@@ -92,15 +130,9 @@
     let loadMore: boolean = true;
 
     function handleLoadMore() {
+        // TODO was passiert, wenn es keine pages mehr gibt?
         pageNumber ++;
         getSearchUsers(pageNumber);
-        checkLoadMore(pageNumber++);
-    }
-
-    function checkLoadMore(pageNumber: number) {
-        getSearchUsers(pageNumber);
-        if (searchUsers) {loadMore = true;}
-        else {loadMore = false;}
     }
 
     function handleLoadLess() {
@@ -111,21 +143,25 @@
 
 <div>
     <div class="dropdown">
-        <input class="input w-1/5 my-3" placeholder="Search for user" bind:value={selectedUsername}/>
-        <ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 max-h-80 flex-nowrap overflow-auto">
-            {#each searchUsers as usr}
-                <li><button on:click|preventDefault={() => onUserClicked(usr)}>{usr.username}</button></li>
-            {/each}
-            <li>
-                {#if loadMore}
-                    <button on:click={handleLoadMore}>load more...</button>
-                {/if}
-                {#if pageNumber > 0}
-                    <button on:click={handleLoadLess}>load less...</button>
-                {/if}
-            </li>
-            
-        </ul>
-        <span>You selected: {selectedUsername}</span>
+        <input class="input w-1/5 my-3" placeholder="Search for user" bind:value={typedUsername} on:click={handleDropDownClick}/>
+        {#if dropdownIsOpen}        
+            <ul on:focusout|stopPropagation={handleDropdownFocusLoss} class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 max-h-80 flex-nowrap overflow-auto">
+                {#each searchUsers as usr}
+                    <li>
+                        <button on:click|preventDefault={() => onUserClicked(usr)}>{usr.username}</button>
+                        <!-- <span>{usr.userInfo}</span> -->
+                    </li>
+                {/each}
+                <li>
+                    {#if loadMore && typedUsername}
+                        <button on:click={handleLoadMore}>show more...</button>
+                    {/if}
+                    {#if pageNumber > 0}
+                        <button on:click={handleLoadLess}>show less...</button>
+                    {/if}
+                </li>
+                
+            </ul>
+        {/if}
     </div>
 </div>
