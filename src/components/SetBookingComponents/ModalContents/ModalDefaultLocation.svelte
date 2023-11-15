997ffe2c99e21bcd5894ca62c1a87d9678834dfe
Kargisa <luca.wolf@outlook.com>
2023-11-15 12:29:23 +0100

    fixed flex in location modal

--- src/components/SetBookingComponents/ModalContents/ModalDefaultLocation.svelte
+++ src/components/SetBookingComponents/ModalContents/ModalDefaultLocation.svelte
@@ -21,16 +21,16 @@
 		});
 	}
 
-	const cBase = 'card p-4 w-modal shadow-xl space-y-4';
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4'; //C-Bat ???????
 </script>
 
 {#if $modalStore[0]}
 	<div class={cBase}>
-		<div>
+		<div class="flex flex-col items-center">
 			<h1 class="text-2xl">Do you want to set {$user.location?.locationname} as your default</h1>
-			<div class="flex">
+			<div>
 				<a
-					class="btn variant-filled-success px-14 justify-self-start"
+					class="btn variant-filled-success px-14"
 					href="."
 					on:click={() => {
 						setAsDefault();
@@ -41,7 +41,7 @@
 					on:click={() => {
 						modalStore.close();
 					}}
-					class="btn variant-filled-error px-14 justify-self-end"
+					class="btn variant-filled-error px-14"
 					href=".">No</a
 				>
 			</div>
