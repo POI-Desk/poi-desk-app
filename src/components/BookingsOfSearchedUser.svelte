ac9e4ed67503c526dc884b5fd5431acb3fe5589e
Sofia <0255@htl.rennweg.at>
2023-11-18 21:43:16 +0100

    added x-button

--- src/components/BookingsOfSearchedUser.svelte
+++ src/components/BookingsOfSearchedUser.svelte
@@ -7,30 +7,32 @@
 
 </script>
 
-{#await getBookings.fetch({ variables: { userid: $searchedUser.pk_userid ?? '' } })}
-    <p>Loading...</p>
-{:then bookings}
-    {#each bookings.data?.getBookingsByUserid as booking}
-        <div>
-            <div class="grid grid-cols-4 grid-rows-4">
-                <div class="col-span-1 row-span-4">Photo</div>
-                <div class="col-span-3">{booking.date}</div>
-                <div class="col-span-3">{booking.desk.floor.building.location.locationname}</div>
-                <!-- <div>{booking.bookingnumber}</div> -->
-                {#if booking.isafternoon && booking.ismorning}
-                <div class="col-span-3">07:00 - 20:00</div>
-                {:else if booking.isafternoon}
-                    <div class="col-span-3">13:00 - 20:00</div>
-                {:else if booking.ismorning}
-                    <div class="col-span-3">07:00 - 13:00</div>
-                {/if}
-                <div>{booking.desk.floor.building.buildingname}</div>
-                <div>{booking.desk.floor.floorname}</div>
-                <div>{booking.desk.desknum}</div>
-                
-                
-            </div>
-        </div>
-    {/each}
-{/await}
 
+<div class="mt-7 gap-2">
+    {#await getBookings.fetch({ variables: { userid: $searchedUser.pk_userid ?? '' } })}
+        <p>Loading...</p>
+    {:then bookings}
+        {#each bookings.data?.getBookingsByUserid ?? [] as booking}
+            <div>
+                <div class="grid grid-cols-4 grid-rows-4 gap-2 border rounded-lg mt-1">
+                    <div class="col-span-1 row-span-4">Photo</div>
+                    <div class="col-span-3">{booking.date}</div>
+                    <div class="col-span-3">{booking.desk?.floor?.building?.location?.locationname}</div>
+                    <!-- <div>{booking.bookingnumber}</div> -->
+                    {#if booking.isafternoon && booking.ismorning}
+                    <div class="col-span-3">07:00 - 20:00</div>
+                    {:else if booking.isafternoon}
+                        <div class="col-span-3">13:00 - 20:00</div>
+                    {:else if booking.ismorning}
+                        <div class="col-span-3">07:00 - 13:00</div>
+                    {/if}
+                    <div>{booking.desk?.floor?.building.buildingname}</div>
+                    <div>{booking.desk?.floor?.floorname}</div>
+                    <div>{booking.desk?.desknum}</div>
+                    
+                    
+                </div>
+            </div>
+        {/each}
+    {/await}
+</div>
--- src/routes/bookingsOfSearchedUser/+page.svelte
+++ src/routes/bookingsOfSearchedUser/+page.svelte
@@ -1,9 +1,21 @@
 <script>
+	import { goto } from "$app/navigation";
+
 
 	import BookingsOfSearchedUser from "$components/BookingsOfSearchedUser.svelte";
-import { searchedUser } from "$lib/searchStore";
+	import { searchedUser } from "$lib/searchStore";
+	import { X, Undo2 } from 'lucide-svelte';
 
 </script>
+
+<button	
+	on:click={() => {
+		goto("/");
+	}}
+	class="absolute right-0 text-black px-8 py-4 rounded-full">
+	<X size=35/>
+</button>
+
 <h1>{$searchedUser.username}</h1>
 
 <BookingsOfSearchedUser></BookingsOfSearchedUser>
