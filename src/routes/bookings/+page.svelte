0bcb95c54b5308012900334d1f9e02864400e020
Tobias Hernandez Perez <9182@htl.rennweg.at>
2024-02-11 15:14:01 +0100

    changed past/current to radio group

--- src/routes/bookings/+page.svelte
+++ src/routes/bookings/+page.svelte
@@ -1,16 +1,22 @@
 <script>
   import BookingsOfUser from "$components/PersonalBookingComponents/BookingsOfUser.svelte";
+  import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
 
   let isCurrentBookings = true;
 </script>
 
-<div class="flex flex-col items-center justify-center">
-
-<!--  TODO RADIO GROUP VERWENDEN-->
+<div class="flex flex-col items-center justify-center pb-16">
   <div class="flex flex-row w-full justify-center p-5">
-    <button class="border-b-2 text-xl font-medium text-primary-900 m-10 p-2 px-10" on:click={() => {isCurrentBookings = true}}>current</button>
-    <button class="border-b-2 text-xl font-medium text-primary-900 m-10 p-2 px-10" on:click={() => {isCurrentBookings = false}}>past</button>
+    <RadioGroup
+      border="none"
+      padding="px-5 py-2"
+      spacing="space-x-96"
+      active="variant-filled-primary"
+      hover="hover:variant-soft-primary"
+    >
+      <RadioItem bind:group={isCurrentBookings} name="current" value={true}>current</RadioItem>
+      <RadioItem bind:group={isCurrentBookings} name="past" value={false}>past</RadioItem>
+    </RadioGroup>
   </div>
-
   <BookingsOfUser {isCurrentBookings}></BookingsOfUser>
 </div>
