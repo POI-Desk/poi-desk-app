5a660e472d0682b866a254eeaff72f54f83fa64f
tobi <tischler.dev@gmail.com>
2024-03-03 15:54:12 +0100

    add styling to user page i guess

--- src/routes/(app)/user/+page.svelte
+++ src/routes/(app)/user/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import { CachePolicy } from '$houdini';
 	import { user } from '$lib/userStore';
-	import { SlideToggle, type ModalSettings, getModalStore, LightSwitch } from "@skeletonlabs/skeleton";
+	import {
+		SlideToggle,
+		type ModalSettings,
+		getModalStore,
+		LightSwitch
+	} from '@skeletonlabs/skeleton';
 	import { Pen } from 'lucide-svelte';
 	import { getUserById } from '$lib/queries/userQuerries';
-	import { goto } from "$app/navigation";
+	import { goto } from '$app/navigation';
 
 	$: getUserById.fetch({ variables: { id: $user?.pk_userid }, policy: CachePolicy.NetworkOnly });
 	$: thisUser = $getUserById.data?.getUserById;
@@ -47,8 +52,8 @@
 
 <a class="btn variant-filled-primary" href="">Go the fuck back</a>
 {#if thisUser}
-	<div class="rounded-3xl bg-red-500 h-1/2 mt-5 p-2 m-2 flex flex-col gap-2">
-		<div class="rounded-3xl flex flex-row bg-green-500 h-1/2 p-2">
+	<div class="rounded-3xl variant-filled-primary h-1/2 mt-5 p-2 m-2 flex flex-col gap-2">
+		<div class="rounded-3xl flex flex-row bg-white text-surface-900 h-1/2 p-2">
 			<div class="w-1/2 flex justify-center items-center">
 				<img class="rounded-full h-full" src="/src/images/cover.jpg" alt="profile picture" />
 			</div>
@@ -63,14 +68,16 @@
 			on:click={async () => {
 				modalStore.trigger(modal);
 			}}
-			class="rounded-3xl bg-green-500 h-1/6 flex flex-row justify-center items-center gap-5"
+			class="rounded-3xl bg-white text-surface-900 h-1/6 flex flex-row justify-center items-center gap-5"
 		>
 			<div>
 				{thisUser?.location?.locationname}
 			</div>
 			<Pen /></button
 		>
-		<div class="rounded-3xl bg-green-500 h-1/6 flex flex-row justify-around items-center">
+		<div
+			class="rounded-3xl bg-white text-surface-900 h-1/6 flex flex-row justify-around items-center"
+		>
 			<div>Light Mode</div>
 			<LightSwitch />
 		</div>
@@ -83,11 +90,11 @@
 		</div>
 		-->
 		Statistics:
-		<div class="rounded-3xl bg-green-500 h-1/6 flex flex-row gap-2">
-			<div class="w-1/2 flex justify-center items-center bg-yellow-500 rounded-3xl">
+		<div class="rounded-3xl h-1/6 flex flex-row gap-2">
+			<div class="w-1/2 flex justify-center items-center bg-white text-surface-900 rounded-3xl">
 				{currentMonthName}
 			</div>
-			<div class="w-1/2 flex justify-center items-center bg-yellow-500 rounded-3xl">
+			<div class="w-1/2 flex justify-center items-center bg-white text-surface-900 rounded-3xl">
 				{amountOfBookingsThisMonth}
 			</div>
 		</div>
@@ -105,13 +112,11 @@
 	</div>
 {/if}
 
-<button
-	class="btn variant-filled-primary"
-	on:click={() => goto("/extendedUser")}>Extended User</button>
+<button class="btn variant-filled-primary" on:click={() => goto('/extendedUser')}
+	>Extended User</button
+>
 
-<button
-	class="btn variant-filled-primary"
-	on:click={() => goto("/admin/maps")}>Admin</button>
+<button class="btn variant-filled-primary" on:click={() => goto('/admin/maps')}>Admin</button>
 
 <style>
 	hr {
