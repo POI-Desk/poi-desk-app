d5a3ad0e0ad6d6f99b9e13476544a2e1cd729df1
Tobias Hernandez Perez <9182@htl.rennweg.at>
2024-03-05 14:51:40 +0100

    fixing merge issues

--- src/lib/queries/deskQueries.ts
+++ src/lib/queries/deskQueries.ts
@@ -4,36 +4,38 @@ export const _getDesksOnFloorVariables = () => {
 	return '';
 };
 
-// export const getDesks = graphql(`
-// 	query getDesksOnFloor($floorid: ID!) @load {
-// 		getDesksOnFloor(floorid: $floorid) {
-// 			pk_deskid
-// 			desknum
-// 			x
-// 			y
-// 			bookings {
-// 				user {
-// 					pk_userid
-// 					username
-// 				}
-// 				date
-// 				ismorning
-// 				isafternoon
-// 			}
-// 			attributes {
-// 				attributename
-// 			}
-// 			floor {
-// 				pk_floorid
-// 				floorname
-// 				building {
-// 					pk_buildingid
-// 					buildingname
-// 				}
-// 			}
-// 		}
-// 	}
-// `);
+export const getDesks = graphql(`
+	query getDesksOnFloor($floorid: ID!) @load {
+		getDesksOnFloor(floorid: $floorid) {
+            pk_deskid
+            desknum
+            x
+            y
+            bookings {
+                user {
+                    pk_userid
+                    username
+                }
+                date
+                ismorning
+                isafternoon
+            }
+            attributes {
+                attributename
+            }
+            map {
+                floor {
+                    pk_floorid
+                    floorname
+                    building {
+                        pk_buildingid
+                        buildingname
+                    }
+                }
+            }
+        }
+	}
+`);
 
 export const getDeskById = graphql(`
   query getDeskById($deskId: ID!){
