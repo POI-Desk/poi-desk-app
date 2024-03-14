acc732476e8600fab7a2445c4b1ad7d68ae73d9b
Kargisa <luca.wolf@outlook.com>
2024-03-14 23:20:06 +0100

    new location route

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,64 +1,14 @@
-import { getBuildingsAndFloorsInLocationForMapStore, graphql } from '$houdini';
-import type { User } from '$lib/types/userTypes';
-import type { PageServerLoad } from './$types';
+import type { User } from '$lib/types/userTypes.js';
+import { redirect } from '@sveltejs/kit';
 
-const buildingsForMap = graphql(`
-	query getBuildingsAndFloorsInLocationForMap($locationId: ID!) {
-		getBuildingsInLocation(locationid: $locationId) {
-			pk_buildingid
-			buildingname
-			floors {
-				pk_floorid
-				floorname
-			}
-		}
-	}
-`);
-
-export const load = (async (event) => {
+export const load = async (event) => {
 	const session = event.locals.getSession() as User;
-	const searchParams = event.url.searchParams;
-
-	const b = new getBuildingsAndFloorsInLocationForMapStore();
-	const res = await b.fetch({
-		variables: { locationId: session.location!.pk_locationid },
-		event
-	});
-
-	const buildings = res.data?.getBuildingsInLocation;
-
-	if (!buildings|| !buildings[0].floors) {
-		return {
-			session,
-			buildings: null
-		};
+	if (session.location) {
+		throw redirect(
+			300,
+			`/${session.location.locationname}?date=${new Date().toISOString().split('T')[0]}`
+		);
 	}
 
-	let building = searchParams.get('building');
-	if (!building) {
-		building = buildings[0].buildingname;
-		searchParams.set('building', building);
-	}
-
-	let floor = searchParams.get('floor');
-	if (!floor) {
-		floor = buildings[0].floors[0].floorname;
-		searchParams.set('floor', floor);
-	}
-
-	let date = searchParams.get('date');
-	if (!date) {
-		date = new Date().toISOString().split('T')[0];
-		searchParams.set('date', date);
-	}
-
-	return {
-		session,
-		buildings: res.data?.getBuildingsInLocation,
-		query: {
-			building,
-			floor,
-			date
-		}
-	};
-}) satisfies PageServerLoad;
+	throw redirect(300, '/locations');
+};
